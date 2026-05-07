9372441d49f9bb8febea834dbb5f3b8cad721cbc
Hajar <hajartanani@gmail.com>
2026-05-07 23:15:13 +0100

    feat: page inscription avec 3 profils et upload CIN

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
