2c5974fb65188d98deefb65fd0bea87d9d365401
Hajar <hajartanani@gmail.com>
2026-05-13 10:03:40 +0100

    feat: add web routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/biens', function () {
+    return view('pages.properties');
+});
+
+Route::get('/biens/{id}', function ($id) {
+    return view('pages.property-detail');
+})->name('property.show');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
